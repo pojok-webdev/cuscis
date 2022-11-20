89f617051bfd26c85fefe1368cf7d75b7ccae2c7
pujie <puji@padi.net.id>
2022-11-20 20:58:20 +0700

    add files

--- js/appSetting.js
+++ js/appSetting.js
@@ -1,6 +1,8 @@
 module.exports = {
     port:20221,
     sql:{
-        host:'localhost',user:'teknis',password:'teknis',database:'teknis'
-    }
+        host:'localhost',user:'root',password:'',database:'teknis'
+    },
+    csvpath:'C:\jsworkspace\cuscis\hehe.csv',
+    //csvpath:'/home/klien/www/jsserver/qiscus/hehe.csv'
 }
